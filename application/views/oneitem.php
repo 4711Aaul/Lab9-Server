5651eeb98cfc38e98d3c93d09aed8fd785247eec
unhouseledutraquist <andrewlbusto@gmail.com>
2017-12-01 13:24:15 -0800

    closes #1

--- application/views/oneitem.php
+++
@@ -1,7 +0,0 @@
-<tr>
-    <td>{id}</td>
-    <td>{task}</td>
-    <td>{size}</td>
-    <td>{group}</td>
-    <td>{status}</td>
-</tr>
--- application/views/oneitemx.php
+++
@@ -1,8 +0,0 @@
-<tr>
-    <!-- MODIFY the next line -->
-<td><a href="/mtce/edit/{id}"><input type="button" value="{id}"/></a></td>
-<td>{task}</td>
-<td>{size}</td>
-<td>{group}</td>
-<td>{status}</td>
-</tr>
